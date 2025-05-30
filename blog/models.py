f69de859ef5b35a4a592e361fe6422899fa889ae
zorell11 <zorell@azet.sk>
2025-05-30 10:56:09 +0200

    Added Post model and admin interface

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=64, blank=False)
+    content = models.TextField(blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    udpated_at = models.DateField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
+    
